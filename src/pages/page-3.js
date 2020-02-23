e7251b312dcc7775b94a1874bf58727360817670
franklam2008 <franklam2008@gmail.com>
2020-02-22 20:20:27 -0500

    2nd commit

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 3</p>
+    <p>About page</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
