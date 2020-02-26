8ef5f8b9978bb0d5d0fe6464d2ddf70494926675
franklam2008 <franklam2008@gmail.com>
2020-02-26 18:01:13 -0500

    footer styled

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,16 @@
 import React from "react"
 import styled from "styled-components"
 import { Container, Icon } from "semantic-ui-react"
+const Footer = styled.div`
+  text-align: center;
+`
 const MadeWith = styled.div`
   color: red;
 `
+
 export default function footer() {
   return (
-    <footer>
+    <Footer>
       <Container>
         © {new Date().getFullYear()},{` `}
         <span>Built with React JS Gatsby</span>
@@ -14,6 +18,6 @@ export default function footer() {
           Made with <Icon name="heart" size="small" /> in Orlando, Florida
         </MadeWith>
       </Container>
-    </footer>
+    </Footer>
   )
 }
