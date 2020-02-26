638d1ba26cc323c27148d36bcace72b7dd110976
franklam2008 <franklam2008@gmail.com>
2020-02-26 18:23:23 -0500

    faq

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -9,6 +9,18 @@ const FAQ = () => (
     <SEO title="FAQ" />
     <h1>Hi from the FAQ</h1>
     <p>Welcome to FAQ.</p>
+    <h1>Prepare The Tea</h1>
+    <h1>When do I drink the tea?</h1>
+    <p>Drink the tea every morning with or without breakfast </p>
+    <h1>How do I make the tea?</h1>
+    <p>
+      Place 1 teabag into a cup of near boiling water for 3-5 minutes. Remove
+      tea bag and enjoy. (Cold or Hot). Visit the Directions page for more
+      information
+    </p>
+    <h1>Can I add flavor to the tea?</h1>
+    <p>Adding honey or lemon will help make the tea taste better. </p>
+
     <Link to="/shop/">Go back to the shop</Link>
   </Layout>
 )
